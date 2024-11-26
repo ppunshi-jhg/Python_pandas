145a8d4e2930a0a69be7ad9f7429679dea12e118
Prince Punshi <ppunshi@jhg.com.au>
2024-11-27 09:24:25 +1000

    adding new repository of python with one file

---
+++ dataTypes.py
@@ -0,0 +1,12 @@
+# int, float, bool, list, set etc
+
+# Type function- type() function is used to print the data type of the value
+
+print(type("123"))
+
+# type conversion- is to convert the data type from one to another, but you cant convert everything, see below for examples
+
+print(int("123"))    #this can be converted easily
+# print(int("abc"))  this cant be converted to integer
+
+# Iterables: are data types which can be iterated, or looped through, allowing you to move from one value to the next- like strings, list etc
