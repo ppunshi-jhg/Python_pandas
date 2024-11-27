68409f94c40b9c3d768ac7203728d7e8aea07dbb
Prince Punshi <ppunshi@jhg.com.au>
2024-11-27 11:30:46 +1000

    adding new files

--- dataTypes.py
+++ dataTypes.py
@@ -10,3 +10,7 @@ print(int("123"))    #this can be converted easily
 # print(int("abc"))  this cant be converted to integer
 
 # Iterables: are data types which can be iterated, or looped through, allowing you to move from one value to the next- like strings, list etc
+
+# Mutable: A data type is mutable if it can be modified after its creation
+# Mutable data type: List, disctionary, sets
+# Immutable: Integers, floats, strings, bool, tuples
---
+++ variables.py
@@ -0,0 +1,8 @@
+# variables are containers used to label and store the values for future reference
+price = 5 # price is the variable here
+
+# delete variables: del keyword will permanently remve variables and other objects
+del price
+print(price)  # It will say price is not defined
+
+# Track variables: To see all the varibales in your code- we have %who and %whose in jupyter notebook, but in visual studio code you can use globals(), locals(), dir()
