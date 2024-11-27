707918fbe32503cc366eb2edab992468d9251aaa
Prince Punshi <ppunshi@jhg.com.au>
2024-11-27 14:56:30 +1000

    adding numeric data file

---
+++ numericData.py
@@ -0,0 +1,27 @@
+# 3 Types of Numeric Data Types- int, float, complex numbers
+# Numeric Type Conversion: int(value)
+
+print(int(1.9999)) # Int convert the float numbers down so the result would be 1, not 2
+
+# You can perform arithmetic operations on numerical data
+print(5+3)
+
+# Division / ----> 5/3= 1.666
+# Floor Division //-----> 5//3= 1 Divides one value by another, then rounds down to the nearest integer
+# Modulo operator % 5%3 = 2 , returns the remainder
+# **Exponention - 5**3= 125
+# pemdas order of operation- parenthesis, exponential, multi and division, addiditon and substraction
+
+# Numeric Functions:
+# Single number functions
+#  1. Round --> round(number, # of digits)
+#  2. abs--> abs(number)
+# Multiple numbers functions
+# 1. sum--> sum(iterable)
+# 2. min, max--> min(iterable)
+
+print(round(3.14444444444444,2))
+print(round(abs(-3.144444444444)))
+print(sum([1,2,3,4]))
+print(max([1,2,3,4]))
+print(min([1,2,3,4]))
